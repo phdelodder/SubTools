e3f2842ca86e7d128bc602adbdaa00aacd4d6ade
Philippe Delodder <lodder@delodder.be>
2014-07-06 08:56:42 +0200

    refactor to use apache cli lib for multisubdownloader arg parser

--- MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/App.java
+++ MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/App.java
@@ -1,53 +1,157 @@
 package org.lodder.subtools.multisubdownloader;
+
 import java.awt.EventQueue;
+import java.io.File;
+import java.util.prefs.Preferences;
 
 import javax.swing.UIManager;
 
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.lodder.subtools.multisubdownloader.settings.SettingsControl;
 import org.lodder.subtools.sublibrary.logging.Level;
 import org.lodder.subtools.sublibrary.logging.Logger;
 
 public class App {
 
-  private final static SettingsControl prefctrl = new SettingsControl();
+	private final static SettingsControl prefctrl = new SettingsControl();
 
-  /**
-   * @param args
-   */
-  public static void main(String[] args) {
-    // Default log level for the program
-    Logger.instance.setLogLevel(Level.INFO);
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		// Default log level for the program
+		Logger.instance.setLogLevel(Level.INFO);
 
-    try {
-      if (prefctrl.getSettings().isAutoUpdateMapping()) {
-        Logger.instance.log("Auto updating mapping ....");
-        prefctrl.updateMappingFromOnline();
-      }
-    } catch (Throwable e) {
-      Logger.instance.error(Logger.stack2String(e));
-    }
+		try {
+			if (prefctrl.getSettings().isAutoUpdateMapping()) {
+				Logger.instance.log("Auto updating mapping ....");
+				prefctrl.updateMappingFromOnline();
+			}
+		} catch (Throwable e) {
+			Logger.instance.error(Logger.stack2String(e));
+		}
 
-    CommandLine cmd = new CommandLine(prefctrl);
-    try {
-      cmd.Parse(args);
-    } catch (Exception e) {
-      Logger.instance.error(e.getMessage());
-    }
-    if (cmd.isGui()) {
-      EventQueue.invokeLater(new Runnable() {
-        public void run() {
-          try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            MainWindow window = new MainWindow(prefctrl);
-            window.setVisible(true);
-          } catch (Exception e) {
-            Logger.instance.error(Logger.stack2String(e));
-          }
-        }
-      });
-    } else {
-      cmd.CheckUpdate();
-      cmd.executeArgs();
-    }
-  }
+		CommandLineParser parser = new GnuParser();
+		HelpFormatter formatter = new HelpFormatter();
+
+		org.apache.commons.cli.CommandLine line = null;
+		try {
+			line = parser.parse(getCLIOptions(), args);
+		} catch (ParseException e) {
+			Logger.instance.error(Logger.stack2String(e));
+		}
+
+		if (line != null) {
+			if (line.hasOption("help")) {
+				formatter.printHelp("MultiSubDownloader", getCLIOptions());
+			} else {
+				if (line.hasOption("debug")) {
+					Logger.instance.setLogLevel(Level.DEBUG);
+				}
+				if (line.hasOption("trace")) {
+					Logger.instance.setLogLevel(Level.ALL);
+				}
+				if (line.hasOption("speedy")) {
+					Preferences preferences = Preferences.userRoot();
+					preferences.putBoolean("speedy", true);
+				} else {
+					Preferences preferences = Preferences.userRoot();
+					preferences.putBoolean("speedy", false);
+				}
+
+				if (line.hasOption("importpreferences")) {
+					File file = new File(
+							line.getOptionValue("importpreferences"));
+					try {
+						if (file != null && file.isFile()) {
+							prefctrl.importPreferences(file);
+						}
+					} catch (Exception e) {
+						Logger.instance.error("executeArgs: importPreferences"
+								+ Logger.stack2String(e));
+					}
+				} else if (line.hasOption("updatefromonlinemapping")) {
+					try {
+						prefctrl.updateMappingFromOnline();
+						prefctrl.store();
+					} catch (Throwable e) {
+						Logger.instance
+								.error("executeArgs: updateFromOnlineMapping"
+										+ Logger.stack2String(e));
+					}
+				} else if (line.hasOption("nogui")) {
+					CommandLine cmd = new CommandLine(prefctrl);
+					if (line.hasOption("folder"))
+						cmd.setFolder(new File(line.getOptionValue("folder")));
+
+					if (line.hasOption("language")) {
+						if (line.getOptionValue("language").equals("nl")
+								|| line.getOptionValue("language").equals("en")) {
+							cmd.setLanguagecode(line.getOptionValue("language"));
+						} else {
+							throw new Exception(
+									"Language code not valid must be 'en' or 'nl' ");
+						}
+					} else {
+						Logger.instance
+								.log("No language given using default: 'nl' ");
+						cmd.setLanguagecode("nl");
+					}
+					cmd.setForce(line.hasOption("force"));
+					cmd.setDownloadall(line.hasOption("downloadall"));
+					cmd.setRecursive(line.hasOption("recursive"));
+					cmd.setSubtitleSelectionDialog(line.hasOption("selection"));
+
+					cmd.CheckUpdate();
+					cmd.Run();
+				} else {
+					EventQueue.invokeLater(new Runnable() {
+						public void run() {
+							try {
+								UIManager.setLookAndFeel(UIManager
+										.getSystemLookAndFeelClassName());
+								MainWindow window = new MainWindow(prefctrl);
+								window.setVisible(true);
+							} catch (Exception e) {
+								Logger.instance.error(Logger.stack2String(e));
+							}
+						}
+					});
+				}
+			}
+		}
+	}
+
+	public static Options getCLIOptions() {
+		/**
+		 * CLI Options
+		 */
+		Options options = new Options();
+		options.addOption("help", false, "print this message");
+		options.addOption("nogui", false, "run in cli mode");
+		options.addOption("R", "recursive", false,
+				"search every folder found in the folder");
+		options.addOption("language", true,
+				"gives the language to search for example: --language nl only nl or en");
+		options.addOption("debug", false, "enables logging");
+		options.addOption("trace", false, "more logging");
+		options.addOption("importpreferences", true, "import preferences");
+		options.addOption("force", false,
+				"force to overwrite the subtitle on disk");
+		options.addOption("folder", false, "folder to search");
+		options.addOption("downloadall", false,
+				"Download all found subs using '-v1' system ");
+		options.addOption("updatefromonlinemapping", false,
+				"Update with the Mappings from online");
+		options.addOption("selection", false,
+				"Subtitle selection possible if multiple subtitles detected");
+		options.addOption("speedy", false, "speed somethings up");
+
+		return options;
+	}
 }
--- MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/CommandLine.java
+++ MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/CommandLine.java
@@ -1,13 +1,12 @@
 package org.lodder.subtools.multisubdownloader;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.prefs.Preferences;
 
 import org.lodder.subtools.multisubdownloader.lib.Actions;
 import org.lodder.subtools.multisubdownloader.lib.control.VideoFileFactory;
 import org.lodder.subtools.multisubdownloader.settings.SettingsControl;
-import org.lodder.subtools.sublibrary.logging.Level;
 import org.lodder.subtools.sublibrary.logging.Listener;
 import org.lodder.subtools.sublibrary.logging.Logger;
 import org.lodder.subtools.sublibrary.model.VideoFile;
@@ -15,135 +14,45 @@ import org.lodder.subtools.sublibrary.model.VideoFile;
 public class CommandLine implements Listener {
 
 	private final SettingsControl prefctrl;
-	private boolean recursive;
-	private boolean parse;
-	private String languagecode;
-	private boolean gui;
-	private boolean help;
-	private boolean importPreferences;
-	private File file;
-	private boolean importexclude;
-	private boolean importmapping;
-	private boolean force;
-	private File folder;
-	private boolean downloadall;
+	private boolean recursive = false;
+	private String languagecode = "";
+	private boolean force = false;
+	private File folder = null;
+	private boolean downloadall = false;
 	private final Actions actions;
-	private boolean updateFromOnlineMapping;
-	private boolean subtitleSelectionDialog;
+	private boolean subtitleSelectionDialog = false;
 
 	public CommandLine(final SettingsControl prefctrl) {
 		Logger.instance.addListener(this);
 		this.prefctrl = prefctrl;
 		actions = new Actions(prefctrl.getSettings(), true);
-		parse = false;
-		gui = true;
-		recursive = false;
-		help = false;
-		importPreferences = false;
-		languagecode = "";
-		file = null;
-		importexclude = false;
-		importmapping = false;
-		force = false;
-		folder = null;
-		updateFromOnlineMapping = false;
-		subtitleSelectionDialog = false;
 	}
 
-	public boolean isGui() {
-		return gui;
-	}
-
-	public void Parse(String[] args) throws Exception {
-		boolean speedy = false;
-		String previousarg = "";
-		for (String arg : args) {
-			if (arg.equals("--help")) {
-				gui = false;
-				help = true;
-			} else if (arg.equals("--nogui")) {
-				gui = false;
-			} else if (arg.equals("--recursive") || arg.equals("-R")) {
-				recursive = true;
-			} else if (arg.equalsIgnoreCase("--debug")) {
-				Logger.instance.setLogLevel(Level.DEBUG);
-			} else if (arg.equalsIgnoreCase("--trace")) {
-				Logger.instance.setLogLevel(Level.ALL);
-			} else if (arg.equalsIgnoreCase("--language")
-					|| arg.equalsIgnoreCase("--folder")) {
-				// do nothing
-			} else if (arg.equalsIgnoreCase("--importpreferences")) {
-				importPreferences = true;
-			} else if (arg.equalsIgnoreCase("--importexclude")) {
-				importexclude = true;
-			} else if (arg.equalsIgnoreCase("--importmapping")) {
-				importmapping = true;
-			} else if (arg.equalsIgnoreCase("--updatefromonlinemapping")) {
-				updateFromOnlineMapping = true;
-			} else if (arg.equalsIgnoreCase("--downloadall")) {
-				downloadall = true;
-			} else if (arg.equalsIgnoreCase("--force")) {
-				force = true;
-			} else if (arg.equalsIgnoreCase("--selection")) {
-				subtitleSelectionDialog = true;
-			} else if (previousarg.equals("--language")) {
-				if (arg.equals("nl") || arg.equals("en")) {
-					languagecode = arg;
-				} else {
-					throw new Exception(
-							"Language code not valid must be 'en' or 'nl' ");
-				}
-			} else if (previousarg.equals("--importpreferences")) {
-				file = new File(arg);
-			} else if (previousarg.equals("--folder")) {
-				folder = new File(arg);
-			} else if (arg.equalsIgnoreCase("--speedy")) {
-				speedy = true;
-			} else {
-				Logger.instance.log("Unknown argument: " + arg);
-			}
-			previousarg = arg;
-		}
-		if (languagecode.equals("") && !gui) {
-			Logger.instance.log("No language given using default: 'nl' ");
-			languagecode = "nl";
-		}
-		Preferences preferences = Preferences.userRoot();
-		preferences.putBoolean("speedy", speedy);
-		parse = true;
-	}
-
-	public List<VideoFile> search() throws Exception {
+	private List<VideoFile> search() throws Exception {
 		List<VideoFile> l = new ArrayList<VideoFile>();
-		if (parse) {
-			List<File> folders = new ArrayList<File>();
-			if (folder == null) {
-				folders.addAll(prefctrl.getSettings()
-						.getDefaultIncomingFolders());
-			} else {
-				folders.add(folder);
-			}
-			
-			for (File f : folders) {
-				List<File> files = actions.getFileListing(f, recursive,
-						languagecode, force);
-				Logger.instance
-						.debug("Files found to process: " + files.size());
-				VideoFile videoFile;
-				for (File file : files) {
-					try {
-						videoFile = VideoFileFactory.get(file, f,
-								prefctrl.getSettings(), languagecode);
-						if (videoFile != null)
-							l.add(videoFile);
-					} catch (Exception e) {
-						Logger.instance.error("Search Process"
-								+ Logger.stack2String(e));
-					}
+		List<File> folders = new ArrayList<File>();
+		if (folder == null) {
+			folders.addAll(prefctrl.getSettings().getDefaultIncomingFolders());
+		} else {
+			folders.add(folder);
+		}
+
+		for (File f : folders) {
+			List<File> files = actions.getFileListing(f, recursive,
+					languagecode, force);
+			Logger.instance.debug("Files found to process: " + files.size());
+			VideoFile videoFile;
+			for (File file : files) {
+				try {
+					videoFile = VideoFileFactory.get(file, f,
+							prefctrl.getSettings(), languagecode);
+					if (videoFile != null)
+						l.add(videoFile);
+				} catch (Exception e) {
+					Logger.instance.error("Search Process"
+							+ Logger.stack2String(e));
 				}
 			}
-		} else {
-			throw new Exception("parse wasn't called");
 		}
 		Logger.instance.debug("found files for doDownload: " + l.size());
 		return l;
@@ -175,61 +84,17 @@ public class CommandLine implements Listener {
 		}
 	}
 
-	public void executeArgs() {
-		if (help) {
-			showHelp();
-		} else if (importexclude) {
-		} else if (importmapping) {
-		} else if (updateFromOnlineMapping) {
-			try {
-				this.prefctrl.updateMappingFromOnline();
-				this.prefctrl.store();
-			} catch (Throwable e) {
-				Logger.instance.error("executeArgs: updateFromOnlineMapping"
-						+ Logger.stack2String(e));
+	public void Run() {
+		List<VideoFile> l;
+		try {
+			l = search();
+			for (VideoFile ef : l) {
+				download(ef);
 			}
-		} else if (importPreferences) {
-			try {
-				if (file != null && file.isFile()) {
-					prefctrl.importPreferences(file);
-				}
-			} catch (Exception e) {
-				Logger.instance.error("executeArgs: importPreferences"
-						+ Logger.stack2String(e));
-			}
-		} else {
-			List<VideoFile> l;
-			try {
-				l = search();
-				for (VideoFile ef : l) {
-					download(ef);
-				}
-			} catch (Exception e) {
-				Logger.instance.error("executeArgs: search"
-						+ Logger.stack2String(e));
-			}
+		} catch (Exception e) {
+			Logger.instance.error("executeArgs: search"
+					+ Logger.stack2String(e));
 		}
-	}
-
-	public void showHelp() {
-		System.out.println("--help\t show this");
-		System.out.println("--nogui\t activated commandline usage");
-		System.out
-				.println("-R, --recursive\t search every folder found in the folder");
-		System.out
-				.println("--language LANG\t gives the language to search for example: --language nl only nl or en");
-		System.out.println("--debug\t enables logging");
-		System.out.println("--importpreferences FILE\t import preferences");
-		System.out.println("--force\t force to overwrite the subtitle on disk");
-		System.out.println("--folder FOLDER\t folder to search");
-		System.out
-				.println("--downloadall\t Download all found subs using '-v1' system ");
-		System.out
-				.println("--updatefromonlinemapping\t Update with the Mappings from online");
-		System.out
-				.println("--selection\t Subtitle selection possible if multiple subtitles detected");
-		// System.out.println("--importexclude FILE\t import excludes");
-		// System.out.println("--importmapping FILE\t import mapping");
 	}
 
 	@Override
@@ -242,6 +107,54 @@ public class CommandLine implements Listener {
 		if (u.checkProgram()) {
 			Logger.instance.log("Update available! : " + u.getUpdateUrl());
 		}
+	}
+
+	public boolean isRecursive() {
+		return recursive;
+	}
+
+	public void setRecursive(boolean recursive) {
+		this.recursive = recursive;
+	}
+
+	public String getLanguagecode() {
+		return languagecode;
+	}
+
+	public void setLanguagecode(String languagecode) {
+		this.languagecode = languagecode;
+	}
+
+	public boolean isForce() {
+		return force;
+	}
+
+	public void setForce(boolean force) {
+		this.force = force;
+	}
+
+	public File getFolder() {
+		return folder;
+	}
+
+	public void setFolder(File folder) {
+		this.folder = folder;
+	}
+
+	public boolean isDownloadall() {
+		return downloadall;
+	}
+
+	public void setDownloadall(boolean downloadall) {
+		this.downloadall = downloadall;
+	}
+
+	public boolean isSubtitleSelectionDialog() {
+		return subtitleSelectionDialog;
+	}
+
+	public void setSubtitleSelectionDialog(boolean subtitleSelectionDialog) {
+		this.subtitleSelectionDialog = subtitleSelectionDialog;
 	}
 
 }
