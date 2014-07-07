e6312ff7d163c0798b4d4262fa878073cb178ce7
Philippe Delodder <lodder@delodder.be>
2014-07-07 09:42:09 +0200

    Refactor arg parsing to use apache cli library

--- SubSort/src/main/java/org/lodder/subtools/subsort/App.java
+++ SubSort/src/main/java/org/lodder/subtools/subsort/App.java
@@ -1,6 +1,13 @@
 package org.lodder.subtools.subsort;
+
 import java.io.File;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.lodder.subtools.sublibrary.logging.Level;
 import org.lodder.subtools.sublibrary.logging.Logger;
 
@@ -8,54 +15,49 @@ public class App {
 
   public static void main(String[] args) {
     Logger.instance.setLogLevel(Level.INFO);
-    boolean remove = false;
-    boolean cleanup = false;
-    boolean rebuildindex = false;
-    boolean removefromarchive = false;
-    String inputfolder = "";
-    String outputfolder = "";
-    String previousarg = "";
-    String toRemove = "";
 
-    for (String arg : args) {
-      if (arg.equals("--removefromarchive")) {
-        removefromarchive = true;
-      }
-      if (previousarg.equals("--removefromarchive")) {
-        toRemove = arg;
-      }
-      if (arg.equals("--remove")) {
-        remove = true;
-      }
-      if (arg.equals("--rebuildindex")) {
-        rebuildindex = true;
-      }
-      if (arg.equals("--inputfolder")) {
+    CommandLineParser parser = new GnuParser();
+    HelpFormatter formatter = new HelpFormatter();
 
-      }
-      if (arg.equals("--outputfolder")) {
+    CommandLine line = null;
 
-      }
-      if (arg.equals("--cleanup")) {
-        cleanup = true;
-      }
-      if (previousarg.equals("--inputfolder")) {
-        inputfolder = arg;
-      }
-      if (previousarg.equals("--outputfolder")) {
-        outputfolder = arg;
-      }
-      previousarg = arg;
+    try {
+      line = parser.parse(getCLIOptions(), args);
+    } catch (ParseException e) {
+      Logger.instance.error(Logger.stack2String(e));
     }
 
-    SortSubtitle sortSubtitle = new SortSubtitle();
-    if (rebuildindex) {
-      sortSubtitle.reBuildIndex(new File(outputfolder));
-    } else if (removefromarchive) {
-      sortSubtitle.removeFromRepo(new File(toRemove), new File(outputfolder));
-    } else {
-      sortSubtitle.run(remove, new File(inputfolder), new File(outputfolder), cleanup);
+    if (line != null) {
+      if (line.hasOption("help")) {
+        formatter.printHelp("MultiSubDownloader", getCLIOptions());
+      } else {
+        SortSubtitle sortSubtitle = new SortSubtitle();
+        if (line.hasOption("rebuildindex")) {
+          sortSubtitle.reBuildIndex(new File(line.getOptionValue("outputfolder")));
+        } else if (line.hasOption("removefromarchive")) {
+          sortSubtitle.removeFromRepo(new File(line.getOptionValue("removefromarchive")), new File(
+              line.getOptionValue("outputfolder")));
+        } else {
+          sortSubtitle.run(line.hasOption("remove"), new File(line.getOptionValue("inputfolder")),
+              new File(line.getOptionValue("outputfolder")), line.hasOption("cleanup"));
+        }
+      }
     }
+  }
 
+  private static Options getCLIOptions() {
+    /**
+     * CLI Options
+     */
+    Options options = new Options();
+    options.addOption("help", false, "print this message");
+    options.addOption("removefromarchive", true, "remove subtitle from archive");
+    options.addOption("remove", false, "remove subtitles when copy to the index folder");
+    options.addOption("rebuildindex", false, "rebuild index, ignoring the current index");
+    options.addOption("inputfolder", true, "the folder to be sorted");
+    options.addOption("outputfolder", true, "the folder containing the index");
+    options.addOption("cleanup", false, "clean up none exising subtitles in index");
+
+    return options;
   }
 }
