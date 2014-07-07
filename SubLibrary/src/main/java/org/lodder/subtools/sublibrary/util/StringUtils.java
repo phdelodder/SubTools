ad9213f000242f880f2ed375c6642ef797abcb33
Philippe Delodder <lodder@delodder.be>
2014-07-07 14:20:10 +0200

    Add new method the remove illegal linux chars for filenames

--- SubLibrary/src/main/java/org/lodder/subtools/sublibrary/util/StringUtils.java
+++ SubLibrary/src/main/java/org/lodder/subtools/sublibrary/util/StringUtils.java
@@ -1,23 +1,29 @@
 package org.lodder.subtools.sublibrary.util;
 
 public class StringUtils {
-    
-    public static String[] join(String[]... arrays) {
-        // calculate size of target array
-        int size = 0;
-        for (String[] array : arrays) {
-          size += array.length;
-        }
 
-        // create list of appropriate size
-        java.util.List<String> list = new java.util.ArrayList<String>(size);
+	public static String[] join(String[]... arrays) {
+		// calculate size of target array
+		int size = 0;
+		for (String[] array : arrays) {
+			size += array.length;
+		}
 
-        // add arrays
-        for (String[] array : arrays) {
-          list.addAll(java.util.Arrays.asList(array));
-        }
+		// create list of appropriate size
+		java.util.List<String> list = new java.util.ArrayList<String>(size);
 
-        // create and return final array
-        return list.toArray(new String[size]);
-    }  
+		// add arrays
+		for (String[] array : arrays) {
+			list.addAll(java.util.Arrays.asList(array));
+		}
+
+		// create and return final array
+		return list.toArray(new String[size]);
+	}
+
+	public static String RemoveIllegalFilenameChars(String s) {
+		s = s.replace("/", "");
+		s = s.replace("\0", "");
+		return s;
+	}
 }
