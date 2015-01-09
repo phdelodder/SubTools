9c9ea7ebaed6e8d56c1f6863cce6c3c93ad531c6
phdelodder <lodder@delodder.be>
2015-01-09 13:41:47 +0100

    Fix issue #4

--- MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/lib/Actions.java
+++ MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/lib/Actions.java
@@ -514,26 +514,17 @@ public class Actions {
   }
 
   public int getAutomaticSubtitleSelection(List<Subtitle> matchingSubs) {
-    Subtitle subtitle;
     Logger.instance.debug("getAutomaticSubtitleSelection: # quality rules: "
         + settings.getQualityRuleList().size());
     Logger.instance.debug("getAutomaticSubtitleSelection: quality rules: "
         + settings.getQualityRuleList().toString());
-    for (String quality : settings.getQualityRuleList()) {
-      Logger.instance.trace("Actions", "getAutomaticSubtitleSelection", "Quality Rule checked: "
-          + quality);
-      for (int i = 0; i < matchingSubs.size(); i++) {
-        subtitle = matchingSubs.get(i);
-        Logger.instance.debug("getAutomaticSubtitleSelection: subtitle quality: "
-            + subtitle.getQuality());
-        Logger.instance.trace("Actions", "getAutomaticSubtitleSelection", "subtitle quality: "
-            + subtitle.getQuality());
-        if (quality.equalsIgnoreCase(subtitle.getQuality())) {
-          // subtitle.getQuality().contains(quality)
-          return i;
-        }
-      }
-    }
+    Logger.instance.trace("Actions", "getAutomaticSubtitleSelection", "First run, using equal");
+    int result = qualityRuleSelectionCompare(matchingSubs, true);
+    if (result > -1) return result;
+
+    Logger.instance.trace("Actions", "getAutomaticSubtitleSelection",
+        "Second run, using word exists in");
+    result = qualityRuleSelectionCompare(matchingSubs, false);
 
     if (settings.isOptionsNoRuleMatchTakeFirst()) {
       Logger.instance.debug("getAutomaticSubtitleSelection: Using taking first rule");
@@ -542,6 +533,28 @@ public class Actions {
       Logger.instance.debug("getAutomaticSubtitleSelection: Nothing found");
       return -1;
     }
+  }
+
+  private int qualityRuleSelectionCompare(List<Subtitle> matchingSubs, boolean equal) {
+    Subtitle subtitle;
+    for (String quality : settings.getQualityRuleList()) {
+      Logger.instance.trace("Actions", "qualityRuleSelectionCompare", "Quality Rule checked: "
+          + quality);
+      for (int i = 0; i < matchingSubs.size(); i++) {
+        subtitle = matchingSubs.get(i);
+        Logger.instance.debug("qualityRuleSelectionCompare: subtitle quality: "
+            + subtitle.getQuality());
+        Logger.instance.trace("Actions", "qualityRuleSelectionCompare", "subtitle quality: "
+            + subtitle.getQuality());
+        if (equal && quality.equalsIgnoreCase(subtitle.getQuality())) return i;
+        if (!equal) {
+          for (String q : quality.split(" ")) {
+            if (subtitle.getQuality().contains(q)) return i;
+          }
+        }
+      }
+    }
+    return -1;
   }
 
   public void download(VideoFile videoFile, Subtitle subtitle, int version) throws Exception {
