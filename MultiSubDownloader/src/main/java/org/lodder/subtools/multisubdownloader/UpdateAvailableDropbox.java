8f6bf6df4c9dae992ef2263d8e48422f8a26027b
phdelodder <lodder@delodder.be>
2015-01-10 06:53:26 +0100

    Reformat code

--- MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/UpdateAvailableDropbox.java
+++ MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/UpdateAvailableDropbox.java
@@ -1,4 +1,5 @@
 package org.lodder.subtools.multisubdownloader;
+
 import java.net.URL;
 
 import org.jsoup.Jsoup;
@@ -13,89 +14,87 @@ import org.lodder.subtools.sublibrary.util.http.HttpClient;
 
 public class UpdateAvailableDropbox {
 
-	private final String url;
-	private String updatedUrl;
-	private final CacheManager ucm;
-	private static long timeout = 900;
-	private final static String programName = "MultiSubDownloader";
-	private final static String extension = "jar";
+  private final String url;
+  private String updatedUrl;
+  private final CacheManager ucm;
+  private static long timeout = 900;
+  private final static String programName = "MultiSubDownloader";
+  private final static String extension = "jar";
 
-	public UpdateAvailableDropbox() {
-		url = "https://www.dropbox.com/sh/1gz18xwzinfgmbl/gDfSgYN1qC";
-		ucm = CacheManager.getURLCache();
-		updatedUrl = "";
-	}
+  public UpdateAvailableDropbox() {
+    url = "https://www.dropbox.com/sh/1gz18xwzinfgmbl/gDfSgYN1qC";
+    ucm = CacheManager.getURLCache();
+    updatedUrl = "";
+  }
 
-	public boolean checkProgram() {
-		try {
-			return check(programName, extension);
-		} catch (Exception e) {
-			Logger.instance.error(Logger.stack2String(e));
-		}
-		return false;
-	}
+  public boolean checkProgram() {
+    try {
+      return check(programName, extension);
+    } catch (Exception e) {
+      Logger.instance.error(Logger.stack2String(e));
+    }
+    return false;
+  }
 
-	public boolean checkMapping() {
-		try {
-			return check("Mapping", "xml");
-		} catch (Exception e) {
-			Logger.instance.error(Logger.stack2String(e));
-		}
+  public boolean checkMapping() {
+    try {
+      return check("Mapping", "xml");
+    } catch (Exception e) {
+      Logger.instance.error(Logger.stack2String(e));
+    }
 
-		return false;
-	}
+    return false;
+  }
 
-	public String getUpdateUrl() {
-		return updatedUrl;
-	}
+  public String getUpdateUrl() {
+    return updatedUrl;
+  }
 
-	private boolean check(String baseName, String extension) {
-		try {
-			String newFoundVersion = ConfigProperties.getInstance().getProperty("version");
-			String source = ucm.fetchAsString(new URL(url), timeout);
-			Document sourceDoc = Jsoup.parse(source);
-			Elements results = sourceDoc.getElementsByClass("filename-link");
-			for (Element result : results) {
-				String href = result.attr("href");
-				if (href.contains(baseName)) {
-					String foundVersion = href
-							.substring(href.lastIndexOf("/") + 1, href.length())
-							.replace(baseName, "")
-							.replace("-v", "").replace("." + extension, "").trim().replace("?dl=0","");
-					int compare = compareVersions(ConfigProperties.getInstance().getProperty("version"), foundVersion);
-					if (compare < 0) {
-						if (compareVersions(newFoundVersion, foundVersion) <= 0) {
-							newFoundVersion = foundVersion;
-							updatedUrl = href;
-						}
-					}
-				}
-			}
-			if (HttpClient.isUrl(updatedUrl)) {
-				return true;
-			}
-		} catch (Exception e) {
-			Logger.instance.error(Logger.stack2String(e));
-		}
+  private boolean check(String baseName, String extension) {
+    try {
+      String newFoundVersion = ConfigProperties.getInstance().getProperty("version");
+      String source = ucm.fetchAsString(new URL(url), timeout);
+      Document sourceDoc = Jsoup.parse(source);
+      Elements results = sourceDoc.getElementsByClass("filename-link");
+      for (Element result : results) {
+        String href = result.attr("href");
+        if (href.contains(baseName)) {
+          String foundVersion =
+              href.substring(href.lastIndexOf("/") + 1, href.length()).replace(baseName, "")
+                  .replace("-v", "").replace("." + extension, "").trim().replace("?dl=0", "");
+          int compare =
+              compareVersions(ConfigProperties.getInstance().getProperty("version"), foundVersion);
+          if (compare < 0) {
+            if (compareVersions(newFoundVersion, foundVersion) <= 0) {
+              newFoundVersion = foundVersion;
+              updatedUrl = href;
+            }
+          }
+        }
+      }
+      if (HttpClient.isUrl(updatedUrl)) {
+        return true;
+      }
+    } catch (Exception e) {
+      Logger.instance.error(Logger.stack2String(e));
+    }
 
-		return false;
-	}
+    return false;
+  }
 
-	private int compareVersions(String str1, String str2) {
-		String[] vals1 = str1.split("\\.");
-		String[] vals2 = str2.split("\\.");
-		int i = 0;
-		while (i < vals1.length && i < vals2.length
-				&& vals1[i].equals(vals2[i])) {
-			i++;
-		}
+  private int compareVersions(String str1, String str2) {
+    String[] vals1 = str1.split("\\.");
+    String[] vals2 = str2.split("\\.");
+    int i = 0;
+    while (i < vals1.length && i < vals2.length && vals1[i].equals(vals2[i])) {
+      i++;
+    }
 
-		if (i < vals1.length && i < vals2.length) {
-			int diff = Integer.valueOf(vals1[i]).compareTo(
-					Integer.valueOf(vals2[i]));
-			return Integer.signum(diff);
-		}
+    if (i < vals1.length && i < vals2.length) {
+      int diff = Integer.valueOf(vals1[i]).compareTo(Integer.valueOf(vals2[i]));
+      return Integer.signum(diff);
+    }
 
-		return Integer.signum(vals1.length - vals2.length);
-	}
+    return Integer.signum(vals1.length - vals2.length);
+  }
 }
