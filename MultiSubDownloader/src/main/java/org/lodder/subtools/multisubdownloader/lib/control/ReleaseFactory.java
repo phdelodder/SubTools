1ea4547ca4b747b842b02c72b6b288f2ef908185
phdelodder <lodder@delodder.be>
2015-01-19 14:12:43 +0100

    Make one factory for controls and entity

--- MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/lib/control/ReleaseFactory.java
+++ MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/lib/control/ReleaseFactory.java
@@ -3,19 +3,25 @@ package org.lodder.subtools.multisubdownloader.lib.control;
 import java.io.File;
 
 import org.lodder.subtools.multisubdownloader.settings.model.Settings;
+import org.lodder.subtools.sublibrary.control.ReleaseParser;
 import org.lodder.subtools.sublibrary.exception.ControlFactoryException;
 import org.lodder.subtools.sublibrary.exception.VideoControlException;
 import org.lodder.subtools.sublibrary.exception.VideoFileParseException;
 import org.lodder.subtools.sublibrary.logging.Logger;
+import org.lodder.subtools.sublibrary.model.MovieRelease;
 import org.lodder.subtools.sublibrary.model.Release;
+import org.lodder.subtools.sublibrary.model.TvRelease;
+import org.lodder.subtools.sublibrary.model.VideoType;
 
 public class ReleaseFactory {
 
+  static final ReleaseParser releaseParser = new ReleaseParser();
+  
   public static Release get(final File file, final File basedir, final Settings settings,
       final String languagecode) throws ControlFactoryException, VideoFileParseException,
       VideoControlException {
     Logger.instance.trace("VideoFileFactory", "get", "");
-    ReleaseControl releaseCtrl = ReleaseControlFactory.getController(file, basedir, settings);
+    ReleaseControl releaseCtrl = ReleaseFactory.getController(file, basedir, settings);
     if (releaseCtrl instanceof TvReleaseControl) {
       if (languagecode.isEmpty()) {
         releaseCtrl.process(settings.getMappingSettings().getMappingList());
@@ -35,6 +41,17 @@ public class ReleaseFactory {
       }
     }
     return releaseCtrl.getVideoFile();
+  }
+
+  public static ReleaseControl getController(File file, File basedir, Settings settings)
+      throws VideoFileParseException, ControlFactoryException {
+    Release release = releaseParser.parse(file, basedir);
+    if (release.getVideoType() == VideoType.EPISODE) {
+      return new TvReleaseControl((TvRelease) release, settings);
+    } else if (release.getVideoType() == VideoType.MOVIE) {
+      return new MovieReleaseControl((MovieRelease) release, settings);
+    }
+    throw new ControlFactoryException("Can't find controller");
   }
 
 }
