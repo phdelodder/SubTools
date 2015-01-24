5e6647e4a2fa984f29aa834e87167be8d65b7b5f
phdelodder <lodder@delodder.be>
2015-01-24 12:50:29 +0100

    Remove unnecessary semi colon

--- SubLibrary/src/main/java/org/lodder/subtools/sublibrary/OsCheck.java
+++ SubLibrary/src/main/java/org/lodder/subtools/sublibrary/OsCheck.java
@@ -17,9 +17,10 @@ public final class OsCheck {
 	 */
 	public enum OSType {
 		Windows, MacOS, Linux, Other
-	};
+	}
 
-	// cached result of OS detection
+
+		// cached result of OS detection
 	protected static OSType detectedOS;
 
 	/**
