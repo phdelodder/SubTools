bcdc27976262a22b9a1f2845f3ca0b441238932f
phdelodder <lodder@delodder.be>
2015-01-31 20:48:20 +0100

    Add TVSubtitles to new ServiceProvider

--- MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/lib/JTVsubtitlesAdapter.java
+++ MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/lib/JTVsubtitlesAdapter.java
@@ -3,17 +3,19 @@ package org.lodder.subtools.multisubdownloader.lib;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.lodder.subtools.multisubdownloader.subtitleproviders.SubtitleProvider;
 import org.lodder.subtools.multisubdownloader.subtitleproviders.tvsubtitles.JTVSubtitlesApi;
 import org.lodder.subtools.multisubdownloader.subtitleproviders.tvsubtitles.model.TVsubtitlesSubtitleDescriptor;
 import org.lodder.subtools.sublibrary.JSubAdapter;
 import org.lodder.subtools.sublibrary.control.ReleaseParser;
 import org.lodder.subtools.sublibrary.logging.Logger;
+import org.lodder.subtools.sublibrary.model.Release;
 import org.lodder.subtools.sublibrary.model.TvRelease;
 import org.lodder.subtools.sublibrary.model.MovieRelease;
 import org.lodder.subtools.sublibrary.model.Subtitle;
 import org.lodder.subtools.sublibrary.model.SubtitleMatchType;
 
-public class JTVsubtitlesAdapter implements JSubAdapter {
+public class JTVsubtitlesAdapter implements JSubAdapter, SubtitleProvider {
 
   private static JTVSubtitlesApi jtvapi;
 
@@ -22,6 +24,20 @@ public class JTVsubtitlesAdapter implements JSubAdapter {
       if (jtvapi == null) jtvapi = new JTVSubtitlesApi();
     } catch (Exception e) {
       Logger.instance.error("API JTVsubtitles INIT: " + e.getCause());
+    }
+  }
+  
+  @Override
+  public String getName() {
+    return "TvSubtitles";
+  }
+
+  @Override
+  public List<Subtitle> search(Release release, String languageCode) {
+    if (release instanceof MovieRelease) {
+      return this.searchSubtitles((MovieRelease) release, languageCode);
+    } else {
+      return this.searchSubtitles((TvRelease) release, languageCode);
     }
   }
 
