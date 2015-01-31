3c619a75606a0b9ddb1ac9bb129922bf820c7471
Robin Houtevelts <robin@houtevelts.com>
2015-02-01 00:07:01 +0100

    Fix nullpointerexception

--- SubLibrary/src/main/java/org/lodder/subtools/sublibrary/model/Subtitle.java
+++ SubLibrary/src/main/java/org/lodder/subtools/sublibrary/model/Subtitle.java
@@ -93,6 +93,7 @@ public class Subtitle {
 	 * @param uploader the uploader to set
 	 */
 	public void setUploader(String uploader) {
+    if(uploader == null) uploader = "";
 		this.uploader = uploader;
 	}
 
