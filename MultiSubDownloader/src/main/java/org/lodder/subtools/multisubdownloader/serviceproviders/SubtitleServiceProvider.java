55a7d2e41dc0fb4d10105e95004f7c025c6eade7
Robin Houtevelts <robin@houtevelts.com>
2015-01-31 16:00:33 +0100

    Create SubtitleProviderStore
    This keeps a list of all registered SubtitleProviders

---
+++ MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/serviceproviders/SubtitleServiceProvider.java
@@ -0,0 +1,25 @@
+package org.lodder.subtools.multisubdownloader.serviceproviders;
+
+import org.lodder.subtools.multisubdownloader.framework.Container;
+import org.lodder.subtools.multisubdownloader.framework.Resolver;
+import org.lodder.subtools.multisubdownloader.framework.service.providers.ServiceProvider;
+import org.lodder.subtools.multisubdownloader.subtitleproviders.SubtitleProviderStore;
+
+public class SubtitleServiceProvider implements ServiceProvider {
+
+  @Override
+  public int getPriority() {
+    return 0;
+  }
+
+  @Override
+  public void register(Container app) {
+    final SubtitleProviderStore store = new SubtitleProviderStore();
+    app.bind("SubtitleProviderStore", new Resolver() {
+      @Override
+      public Object resolve() {
+        return store;
+      }
+    });
+  }
+}
