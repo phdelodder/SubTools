5613ec0205fda72bf98f8b24d52647dae4d2ad40
phdelodder <lodder@delodder.be>
2015-01-31 20:48:08 +0100

    Add SubsMax to new ServiceProvider

--- MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/lib/JSubsMaxAdapter.java
+++ MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/lib/JSubsMaxAdapter.java
@@ -3,17 +3,19 @@ package org.lodder.subtools.multisubdownloader.lib;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.lodder.subtools.multisubdownloader.subtitleproviders.SubtitleProvider;
 import org.lodder.subtools.multisubdownloader.subtitleproviders.subsmax.JSubsMaxApi;
 import org.lodder.subtools.multisubdownloader.subtitleproviders.subsmax.model.SubMaxSubtitleDescriptor;
 import org.lodder.subtools.sublibrary.JSubAdapter;
 import org.lodder.subtools.sublibrary.control.ReleaseParser;
 import org.lodder.subtools.sublibrary.logging.Logger;
+import org.lodder.subtools.sublibrary.model.Release;
 import org.lodder.subtools.sublibrary.model.TvRelease;
 import org.lodder.subtools.sublibrary.model.MovieRelease;
 import org.lodder.subtools.sublibrary.model.Subtitle;
 import org.lodder.subtools.sublibrary.model.SubtitleMatchType;
 
-public class JSubsMaxAdapter implements JSubAdapter {
+public class JSubsMaxAdapter implements JSubAdapter, SubtitleProvider  {
 
   private static JSubsMaxApi jsmapi;
 
@@ -24,6 +26,20 @@ public class JSubsMaxAdapter implements JSubAdapter {
       }
     } catch (Exception e) {
       Logger.instance.error("API JSubsMax INIT: " + e.getCause());
+    }
+  }
+  
+  @Override
+  public String getName() {
+    return "SubsMax";
+  }
+
+  @Override
+  public List<Subtitle> search(Release release, String languageCode) {
+    if (release instanceof MovieRelease) {
+      return this.searchSubtitles((MovieRelease) release, languageCode);
+    } else {
+      return this.searchSubtitles((TvRelease) release, languageCode);
     }
   }
 
