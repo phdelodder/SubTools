64d66ebad9fa9bf3bdeb1e80abf7dda3cf5882fe
Robin Houtevelts <robin@houtevelts.com>
2015-01-31 16:06:36 +0100

    Refactor SearchActions
    To use the new SearchManager

--- MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/gui/actions/search/FileSearchAction.java
+++ MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/gui/actions/search/FileSearchAction.java
@@ -1,62 +1,106 @@
 package org.lodder.subtools.multisubdownloader.gui.actions.search;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import org.lodder.subtools.multisubdownloader.MainWindow;
+import org.lodder.subtools.multisubdownloader.gui.actions.ActionException;
+import org.lodder.subtools.multisubdownloader.gui.extra.table.VideoTableModel;
 import org.lodder.subtools.multisubdownloader.gui.panels.SearchFileInputPanel;
-import org.lodder.subtools.multisubdownloader.gui.workers.SearchFileWorker;
+import org.lodder.subtools.multisubdownloader.lib.Actions;
 import org.lodder.subtools.multisubdownloader.settings.model.Settings;
-import org.lodder.subtools.sublibrary.logging.Logger;
+import org.lodder.subtools.multisubdownloader.subtitleproviders.SubtitleProviderStore;
+import org.lodder.subtools.sublibrary.control.ReleaseParser;
+import org.lodder.subtools.sublibrary.exception.VideoFileParseException;
+import org.lodder.subtools.sublibrary.model.Release;
+import org.lodder.subtools.sublibrary.model.Subtitle;
 
 public class FileSearchAction extends SearchAction {
 
-  public FileSearchAction(MainWindow mainWindow, Settings settings) {
-    super(mainWindow, settings);
+  private Actions actions;
+  private ReleaseParser releaseParser;
+
+  public FileSearchAction(MainWindow mainWindow, Settings settings, SubtitleProviderStore subtitleProviderStore) {
+    super(mainWindow, settings, subtitleProviderStore);
   }
 
-  protected void createSearchWorker() {
-    // Todo: extract to factory?
-    setWorker(new SearchFileWorker(this.searchPanel.getResultPanel().getTable(),this.settings));
+  public void setActions(Actions actions) {
+    this.actions = actions;
+  }
 
+  public void setReleaseParser(ReleaseParser releaseParser) {
+    this.releaseParser = releaseParser;
+  }
+
+  @Override
+  protected List<Release> createReleases() throws ActionException {
     SearchFileInputPanel inputPanel = getInputPanel();
-    SearchFileWorker worker = getWorker();
-
-    Logger.instance.trace(this.getClass().toString(), "createSearchWorker", "Setting parameters for searchWorker");
-
     String filePath = inputPanel.getIncomingPath();
     String languageCode = getLanguageCode(inputPanel.getSelectedLanguage());
     boolean recursive = inputPanel.isRecursiveSelected();
     boolean overwriteExistingSubtitles = inputPanel.isForceOverwrite();
 
-    if(!filePath.equals("")) {
-      Logger.instance.debug("SearchWorker Parameter: dirs => '"+ filePath +"'");
-      worker.setDirs(new File(filePath));
-    }else if(this.settings.hasDefaultFolders()) {
-      Logger.instance.debug("SearchWorker Parameter: dirs => 'defaultFolders'");
-      worker.setDirs(this.settings.getDefaultFolders());
+    /* get a list of videofiles */
+    List<File> files = getFiles(filePath, languageCode, recursive, overwriteExistingSubtitles);
+
+    /* create a list of releases from videofiles */
+    return createReleases(files);
+  }
+
+  private List<Release> createReleases(List<File> files) throws ActionException {
+    /* parse every videofile */
+    List<Release> releases = new ArrayList<>();
+    try {
+      for (File file : files) {
+        releases.add(this.releaseParser.parse(file, file.getParentFile()));
+      }
+    } catch (VideoFileParseException e) {
+      throw new ActionException(e);
     }
 
-    Logger.instance.debug("SearchWorker Parameter: langcode => '"+ languageCode +"'");
-    Logger.instance.debug("SearchWorker Parameter: isrecursice => '"+ recursive +"'");
-    Logger.instance.debug("SearchWorker Parameter: overwritesubtitles => '"+ overwriteExistingSubtitles +"'");
+    return releases;
+  }
 
-    worker.setLanguageCode(languageCode);
-    worker.setRecursive(recursive);
-    worker.setOverwrite(overwriteExistingSubtitles);
+  private List<File> getFiles(String filePath, String languageCode, boolean recursive, boolean overwriteExistingSubtitles) {
+    /* Get a list of selected directories */
+    List<File> dirs = new ArrayList<>();
+    if (!filePath.isEmpty()) {
+      dirs.add(new File(filePath));
+    } else {
+      dirs.addAll(this.settings.getDefaultFolders());
+    }
+
+    /* Scan directories for videofiles */
+    List<File> files = new ArrayList<>();
+    for (File dir : dirs) {
+      files.addAll(this.actions.getFileListing(dir, recursive, languageCode, overwriteExistingSubtitles));
+    }
+    return files;
+  }
+
+  @Override
+  public void onFoundSubtitles(Release release, List<Subtitle> subtitles) {
+    VideoTableModel model = (VideoTableModel) this.searchPanel.getResultPanel().getTable().getModel();
+
+    release.getMatchingSubs().addAll(subtitles);
+
+    model.addRow(release);
+  }
+
+  @Override
+  protected void postFound() {
+
   }
 
   protected void inputCheck() throws SearchSetupException {
     String path = getInputPanel().getIncomingPath();
 
-    if(path.equals("") && !this.settings.hasDefaultFolders())
+    if (path.equals("") && !this.settings.hasDefaultFolders())
       throw new SearchSetupException("Geen map geselecteerd");
   }
 
   private SearchFileInputPanel getInputPanel() {
     return (SearchFileInputPanel) this.searchPanel.getInputPanel();
-  }
-
-  private SearchFileWorker getWorker() {
-    return (SearchFileWorker) this.worker;
   }
 
 }
--- MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/gui/actions/search/SearchAction.java
+++ MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/gui/actions/search/SearchAction.java
@@ -1,42 +1,37 @@
 package org.lodder.subtools.multisubdownloader.gui.actions.search;
 
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import javax.swing.SwingWorker;
+import java.util.List;
 import org.lodder.subtools.multisubdownloader.MainWindow;
 import org.lodder.subtools.multisubdownloader.gui.actions.ActionException;
+import org.lodder.subtools.multisubdownloader.gui.dialog.Cancelable;
+import org.lodder.subtools.multisubdownloader.gui.extra.table.VideoTableModel;
 import org.lodder.subtools.multisubdownloader.gui.panels.SearchPanel;
+import org.lodder.subtools.multisubdownloader.gui.workers.SearchHandler;
+import org.lodder.subtools.multisubdownloader.gui.workers.SearchManager;
 import org.lodder.subtools.multisubdownloader.settings.model.Settings;
+import org.lodder.subtools.multisubdownloader.subtitleproviders.SubtitleProvider;
+import org.lodder.subtools.multisubdownloader.subtitleproviders.SubtitleProviderStore;
 import org.lodder.subtools.sublibrary.logging.Logger;
+import org.lodder.subtools.sublibrary.model.Release;
+import org.lodder.subtools.sublibrary.model.Subtitle;
 
-public abstract class SearchAction {
+public abstract class SearchAction implements Cancelable, SearchHandler {
 
   protected MainWindow mainWindow;
   protected Settings settings;
+  protected SubtitleProviderStore subtitleProviderStore;
   protected SearchPanel searchPanel;
-  protected SwingWorker<?, ?> worker;
+  protected SearchManager searchManager;
+  protected List<Release> releases;
 
-  public SearchAction(MainWindow mainWindow, Settings settings) {
+  public SearchAction(MainWindow mainWindow, Settings settings, SubtitleProviderStore subtitleProviderStore) {
     this.mainWindow = mainWindow;
     this.settings = settings;
+    this.subtitleProviderStore = subtitleProviderStore;
   }
 
-  public void setSearchPanel(SearchPanel searchPanel){
+  public void setSearchPanel(SearchPanel searchPanel) {
     this.searchPanel = searchPanel;
-  }
-
-  public void setWorker(final SwingWorker<?, ?> worker) {
-    this.worker = worker;
-    this.worker.addPropertyChangeListener(new PropertyChangeListener() {
-      @Override
-      public void propertyChange(PropertyChangeEvent evt) {
-        if (worker.isDone()) {
-          searchCompleted();
-        } else {
-          searchProgressed();
-        }
-      }
-    });
   }
 
   public void execute() {
@@ -51,7 +46,31 @@ public abstract class SearchAction {
   public void search() throws ActionException {
     inputCheck();
 
-    createSearchWorker();
+    String languageCode = this.getLanguageCode(this.searchPanel.getInputPanel().getSelectedLanguage());
+
+    this.releases = createReleases();
+
+    if (this.releases.size() <= 0)
+      return;
+
+    /* Create a new SearchManager. */
+    this.searchManager = new SearchManager(this.settings);
+
+    /* Tell the manager which language we want */
+    this.searchManager.setLanguage(languageCode);
+
+    /* Tell the manager which providers to use */
+    for (SubtitleProvider subtitleProvider : this.subtitleProviderStore.getAllProviders()) {
+      this.searchManager.addProvider(subtitleProvider);
+    }
+
+    /* Tell the manager which releases to search. */
+    for (Release release : this.releases) {
+      this.searchManager.addRelease(release);
+    }
+
+    /* Listen for when the manager tells us Subtitles are found */
+    this.searchManager.onFound(this);
 
     showProgressDialog();
 
@@ -61,17 +80,47 @@ public abstract class SearchAction {
 
     this.searchPanel.getResultPanel().clearTable();
 
-    this.worker.execute();
+    /* Tell the manager to start searching */
+    this.searchManager.start();
   }
+
+  protected abstract List<Release> createReleases() throws ActionException;
+
+  protected abstract void onFoundSubtitles(Release release, List<Subtitle> subtitles);
 
   protected void setStatusMessage(String message) {
     this.mainWindow.setStatusMessage(message);
   }
 
   protected void showProgressDialog() {
-    this.mainWindow.setProgressDialog(this.worker);
+    this.mainWindow.setProgressDialog(this);
     this.mainWindow.showProgressDialog();
   }
+
+  @Override
+  public boolean cancel(boolean mayInterruptIfRunning) {
+    return this.searchManager.cancel(mayInterruptIfRunning);
+  }
+
+  @Override
+  public void onFound(Release release, List<Subtitle> subtitles) {
+    this.onFoundSubtitles(release, subtitles);
+
+    VideoTableModel model = (VideoTableModel) this.searchPanel.getResultPanel().getTable().getModel();
+
+    if(model.getRowCount() > 0) {
+      searchPanel.getResultPanel().enableButtons();
+    }
+
+    if (this.searchManager.getProgress() == 100) {
+      mainWindow.hideProgressDialog();
+      searchPanel.getInputPanel().enableSearchButton();
+    }
+
+    this.postFound();
+  }
+
+  protected abstract void postFound();
 
   protected String getLanguageCode(String language) {
     if (language.equals("Nederlands")) {
@@ -82,38 +131,6 @@ public abstract class SearchAction {
     return null;
   }
 
-  protected abstract void createSearchWorker();
-
   protected abstract void inputCheck() throws SearchSetupException;
 
-  protected void searchCompleted() {
-    Logger.instance.trace(this.getClass().toString(), "searchCompleted", "SearchWorker has completed");
-
-    int foundSubtitles = searchPanel.getResultPanel().getTable().getModel().getRowCount();
-    Logger.instance.debug("Found " + foundSubtitles + " subtitles");
-
-    if (foundSubtitles > 0) {
-      searchPanel.getResultPanel().enableButtons();
-    }
-
-    setStatusMessage("Found " + foundSubtitles + " subtitles");
-
-    mainWindow.hideProgressDialog();
-    searchPanel.getInputPanel().enableSearchButton();
-  }
-
-  protected void searchProgressed() {
-    Logger.instance.trace(this.getClass().toString(), "searchProgressed", "SearchWorker is busy");
-
-    int progress = worker.getProgress();
-    Logger.instance.debug("Progress: "+progress);
-
-    mainWindow.updateProgressDialog(progress);
-
-    if (progress == 0) {
-      setStatusMessage("Bestanden lijst aan het opbouwen");
-    } else {
-      setStatusMessage("Bestanden aan het verwerken");
-    }
-  }
 }
