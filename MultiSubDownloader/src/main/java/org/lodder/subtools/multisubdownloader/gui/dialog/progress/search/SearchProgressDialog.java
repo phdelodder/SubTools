26ec2b70408b595cacbe4b734eecce05793a5e76
Robin Houtevelts <robin@houtevelts.com>
2015-02-06 00:17:31 +0100

    Create GUI SearchProgressDialog

---
+++ MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/gui/dialog/progress/search/SearchProgressDialog.java
@@ -0,0 +1,78 @@
+package org.lodder.subtools.multisubdownloader.gui.dialog.progress.search;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.*;
+import net.miginfocom.swing.MigLayout;
+import org.lodder.subtools.multisubdownloader.gui.dialog.Cancelable;
+import org.lodder.subtools.multisubdownloader.gui.dialog.MultiSubDialog;
+import org.lodder.subtools.multisubdownloader.subtitleproviders.SubtitleProvider;
+import org.lodder.subtools.sublibrary.model.Release;
+
+public class SearchProgressDialog extends MultiSubDialog implements SearchProgressListener {
+
+  private final Cancelable searchAction;
+  private SearchProgressTableModel tableModel;
+  private JProgressBar progressBar;
+
+  public SearchProgressDialog(JFrame frame, Cancelable searchAction) {
+    super(frame, "Searching", false);
+    this.searchAction = searchAction;
+
+    initialize_ui();
+    setDialogLocation(frame);
+    repaint();
+    this.setVisible(true);
+  }
+
+  @Override
+  public void progress(SubtitleProvider provider, int jobsLeft, Release release) {
+    this.tableModel.update(provider.getName(), jobsLeft, (release == null ? "Done" : release.getFilename()));
+  }
+
+  @Override
+  public void progress(int progress) {
+    if (progress == 0) {
+      this.progressBar.setIndeterminate(true);
+    } else {
+      this.progressBar.setIndeterminate(false);
+      this.progressBar.setValue(progress);
+      this.progressBar.setString(Integer.toString(progress));
+    }
+  }
+
+  private void initialize_ui() {
+    addWindowListener(new WindowAdapter() {
+      @Override
+      public void windowClosing(WindowEvent e) {
+        searchAction.cancel(true);
+      }
+    });
+    setBounds(100, 100, 601, 300);
+    getContentPane().setLayout(new MigLayout());
+
+    this.tableModel = new SearchProgressTableModel();
+    JTable table = new JTable(tableModel);
+
+    table.getColumnModel().getColumn(0).setMaxWidth(150);
+    table.getColumnModel().getColumn(1).setMaxWidth(50);
+
+    JScrollPane tablePane = new JScrollPane(table);
+    tablePane.setViewportView(table);
+    getContentPane().add(tablePane, "wrap");
+
+    progressBar = new JProgressBar(0, 100);
+    progressBar.setIndeterminate(true);
+    getContentPane().add(progressBar, "grow, wrap");
+
+    JButton btnStop = new JButton("Stop!");
+    btnStop.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent arg0) {
+        searchAction.cancel(true);
+      }
+    });
+    getContentPane().add(btnStop, "alignx left");
+  }
+}
---
+++ MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/gui/dialog/progress/search/SearchProgressTableModel.java
@@ -0,0 +1,41 @@
+package org.lodder.subtools.multisubdownloader.gui.dialog.progress.search;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.table.DefaultTableModel;
+
+public class SearchProgressTableModel extends DefaultTableModel {
+
+  private String[] columnNames = {"Source", "Queue", "Release"};
+  private Map<String, Integer> rowMap = new HashMap<>();
+
+  public SearchProgressTableModel() {
+    super();
+    this.setColumnCount(3);
+    this.setColumnIdentifiers(columnNames);
+  }
+
+  public void update(String source, int queue, String release) {
+    if (!this.rowMap.containsKey(source)) {
+      /* Source has no row, so create row */
+      createRow(source, queue, release);
+      return;
+    }
+
+    /* SubtitleSource has a row, update row */
+    int rowNr = this.rowMap.get(source);
+    updateRow(rowNr, queue, release);
+  }
+
+  private void updateRow(int rowNr, int queue, String release) {
+    this.setValueAt(queue, rowNr, 1); // Queue
+    this.setValueAt(release, rowNr, 2); // Release
+  }
+
+  private void createRow(String source, int queue, String release) {
+    this.rowMap.put(source, this.getRowCount());
+
+    Object[] row = {source, queue, release};
+    this.addRow(row);
+  }
+}
