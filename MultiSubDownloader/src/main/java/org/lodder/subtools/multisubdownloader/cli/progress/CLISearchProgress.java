910ad99051d666945762bab4d42604bb176f4328
phdelodder <lodder@delodder.be>
2015-02-12 06:10:05 +0100

    Move CLISearchProgress to correct package

--- MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/cli/progress/CLISearchProgress.java
+++ MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/cli/progress/CLISearchProgress.java
@@ -1,6 +1,7 @@
-package org.lodder.subtools.multisubdownloader.gui.dialog.progress.search;
+package org.lodder.subtools.multisubdownloader.cli.progress;
 
 import org.lodder.subtools.multisubdownloader.actions.ActionException;
+import org.lodder.subtools.multisubdownloader.gui.dialog.progress.search.SearchProgressTableModel;
 import org.lodder.subtools.multisubdownloader.listeners.SearchProgressListener;
 import org.lodder.subtools.multisubdownloader.subtitleproviders.SubtitleProvider;
 import org.lodder.subtools.sublibrary.model.Release;
