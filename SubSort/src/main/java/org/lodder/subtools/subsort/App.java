d7dbf3818846b8c0c921c94bd00c40d3c4934f14
Philippe Delodder <lodder@delodder.be>
2015-03-09 15:12:44 +0100

    Convert App from old logger to new LOGGER slf4j

--- SubSort/src/main/java/org/lodder/subtools/subsort/App.java
+++ SubSort/src/main/java/org/lodder/subtools/subsort/App.java
@@ -8,13 +8,17 @@ import org.apache.commons.cli.GnuParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
-import org.lodder.subtools.sublibrary.logging.Level;
-import org.lodder.subtools.sublibrary.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Level;
 
 public class App {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+  
   public static void main(String[] args) {
-    Logger.instance.setLogLevel(Level.INFO);
+    setLoggingLevel(Level.INFO);
 
     CommandLineParser parser = new GnuParser();
     HelpFormatter formatter = new HelpFormatter();
@@ -24,7 +28,7 @@ public class App {
     try {
       line = parser.parse(getCLIOptions(), args);
     } catch (ParseException e) {
-      Logger.instance.error(Logger.stack2String(e));
+      LOGGER.error("Unable to parse cli options", e);
     }
 
     if (line != null) {
@@ -44,6 +48,11 @@ public class App {
       }
     }
   }
+  
+  public static void setLoggingLevel(Level level) {
+    ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory.getLogger(ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME);
+    root.setLevel(level);
+}
 
   private static Options getCLIOptions() {
     /**
