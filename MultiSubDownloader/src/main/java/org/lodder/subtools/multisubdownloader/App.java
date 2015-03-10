3c6e81dccb149d4165888678b2682ab0bd44f9cd
Philippe Delodder <lodder@delodder.be>
2015-03-10 11:00:18 +0100

    Clean up imports

--- MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/App.java
+++ MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/App.java
@@ -28,11 +28,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
-import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.ConsoleAppender;
-import ch.qos.logback.core.FileAppender;
 
 public class App {
 
