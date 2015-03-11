8c9c46ffe42143b9731738ff006c8c2966639f72
Philippe Delodder <lodder@delodder.be>
2015-03-11 09:25:41 +0100

    Add proper cli selection of subtitles

--- MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/lib/SubtitleSelectionCLI.java
+++ MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/lib/SubtitleSelectionCLI.java
@@ -1,10 +1,11 @@
 package org.lodder.subtools.multisubdownloader.lib;
 
 import java.io.Console;
-import java.util.List;
 
 import org.lodder.subtools.multisubdownloader.settings.model.Settings;
 import org.lodder.subtools.sublibrary.model.Release;
+
+import dnl.utils.text.table.TextTable;
 
 public class SubtitleSelectionCLI extends SubtitleSelection {
 
@@ -15,10 +16,23 @@ public class SubtitleSelectionCLI extends SubtitleSelection {
   @Override
   public int getUserInput(Release release) {
     System.out.println("\nSelect best subtitle for : " + release.getFilename());
-    List<String> lines = this.buildDisplayLines(release);
-    for (int i = 0; i < lines.size(); i++) {
-      System.out.println("(" + i + ") " + lines.get(i));
+    String[] columnNames = {"Score", "Subtitle name", "Source", "Uploader", "Hearing impaired"};
+
+    Object[][] dataTable = new Object[release.getMatchingSubs().size()][columnNames.length];
+    for (int i = 0; i < release.getMatchingSubs().size(); i++) {
+      dataTable[i] =
+          new Object[] {release.getMatchingSubs().get(i).getScore(),
+              release.getMatchingSubs().get(i).getFilename(),
+              release.getMatchingSubs().get(i).getSubtitleSource(),
+              release.getMatchingSubs().get(i).getUploader(),
+              release.getMatchingSubs().get(i).isHearingImpaired()};
     }
+
+    TextTable tt = new TextTable(columnNames, dataTable);
+    // this adds the numbering on the left
+    tt.setAddRowNumbering(true);
+    tt.printTable();
+
     System.out.println("(-1) To skip download and/or move!");
     Console c = System.console();
     String selectedSubtitle = c.readLine("Enter number of selected subtitle: ");
