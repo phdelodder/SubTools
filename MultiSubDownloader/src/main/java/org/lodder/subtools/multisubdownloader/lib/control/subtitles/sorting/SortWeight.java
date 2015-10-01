e2a0151b97c1e50a92a67980832dae301d7c3707
Philippe Delodder <lodder@delodder.be>
2015-10-01 15:44:30 +0200

    No use for protected replace with private

--- MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/lib/control/subtitles/sorting/SortWeight.java
+++ MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/lib/control/subtitles/sorting/SortWeight.java
@@ -11,7 +11,7 @@ import org.lodder.subtools.sublibrary.model.Release;
 
 public class SortWeight {
 
-  protected static final List<KeywordReplacer> keywordReplacers;
+  private static final List<KeywordReplacer> keywordReplacers;
 
   static {
     keywordReplacers = new ArrayList<>();
