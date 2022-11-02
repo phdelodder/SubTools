edbe533c75d835f1ecdbc6ad8af162b3d84ed921
EotT123 <EotT123>
2022-11-02 11:29:26 +0100

    improve localization when changing language

--- MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/cli/CliOption.java
+++ MultiSubDownloader/src/main/java/org/lodder/subtools/multisubdownloader/cli/CliOption.java
@@ -8,29 +8,33 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum CliOption {
-    HELP("help", false, Messages.getString("App.OptionHelpMsg")),
-    NO_GUI("nogui", false, Messages.getString("App.OptionNoGuiMsg")),
-    RECURSIVE("R", "recursive", false, Messages.getString("App.OptionOptionRecursiveMsg")),
-    LANGUAGE("language", true, Messages.getString("App.OptionOptionLanguageMsg")),
-    DEBUG("debug", false, Messages.getString("App.OptionOptionDebugMsg")),
-    TRACE("trace", false, Messages.getString("App.OptionOptionTraceMsg")),
-    IMPORT_PREFERENCES("importpreferences", true, Messages.getString("App.OptionOptionImportPreferencesMsg")),
-    FORCE("force", false, Messages.getString("App.OptionOptionForceMsg")),
-    FOLDER("folder", true, Messages.getString("App.OptionOptionFolderMsg")),
-    DOWNLOAD_ALL("downloadall", false, Messages.getString("App.OptionOptionDownloadAllMsg")),
-    SELECTION("selection", false, Messages.getString("App.OptionOptionSelectionMsg")),
-    SPEEDY("speedy", false, Messages.getString("App.OptionOptionSpeedyMsg")),
-    VERBOSE_PROGRESS("verboseprogress", false, Messages.getString("App.OptionVerboseProgressCLI")),
-    DRY_RUN("dryrun", false, Messages.getString("App.OptionDryRun")),
-    CONFIRM_PROVIDER_MAPPING("confirmProviderMapping", false, Messages.getString("App.OptionConfirmProviderMapping"));
+    HELP("help", false, "App.OptionHelpMsg"),
+    NO_GUI("nogui", false, "App.OptionNoGuiMsg"),
+    RECURSIVE("R", "recursive", false, "App.OptionOptionRecursiveMsg"),
+    LANGUAGE("language", true, "App.OptionOptionLanguageMsg"),
+    DEBUG("debug", false, "App.OptionOptionDebugMsg"),
+    TRACE("trace", false, "App.OptionOptionTraceMsg"),
+    IMPORT_PREFERENCES("importpreferences", true, "App.OptionOptionImportPreferencesMsg"),
+    FORCE("force", false, "App.OptionOptionForceMsg"),
+    FOLDER("folder", true, "App.OptionOptionFolderMsg"),
+    DOWNLOAD_ALL("downloadall", false, "App.OptionOptionDownloadAllMsg"),
+    SELECTION("selection", false, "App.OptionOptionSelectionMsg"),
+    SPEEDY("speedy", false, "App.OptionOptionSpeedyMsg"),
+    VERBOSE_PROGRESS("verboseprogress", false, "App.OptionVerboseProgressCLI"),
+    DRY_RUN("dryrun", false, "App.OptionDryRun"),
+    CONFIRM_PROVIDER_MAPPING("confirmProviderMapping", false, "App.OptionConfirmProviderMapping");
 
     private final String value;
     private final String longValue;
     private final boolean hasArg;
-    private final String description;
+    private final String msgCode;
 
     CliOption(String value, boolean hasArg, String description) {
         this(value, null, hasArg, description);
+    }
+
+    public String getDescription() {
+        return Messages.getString(msgCode);
     }
 
 }
